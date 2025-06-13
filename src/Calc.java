48c28512c1cb0b97008a1a1e8918f326fc47cf3d
YuusukeNakajima <yuu.rzsui@gmail.com>
2025-06-13 16:09:40 +0900

    printAdd()メソッドを追加

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,15 @@
+import static java.lang.System.*;
 
 public class Calc {
 
 	public static void main(String[] args) {
-		
+		printAdd(10,15);
 
+	}
+	
+	public static void printAdd(int arg1, int arg2) {
+//		引数で与えられた数値の合計を出力する
+		out.println(arg1 + arg2);
 	}
 
 }
