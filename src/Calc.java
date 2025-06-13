0fe8bb3ea14d5245b6c723a8c79cc0e3b3ec8b99
YuusukeNakajima <yuu.rzsui@gmail.com>
2025-06-13 16:13:39 +0900

    printSub()メソッドを追加

--- src/Calc.java
+++ src/Calc.java
@@ -3,13 +3,20 @@ import static java.lang.System.*;
 public class Calc {
 
 	public static void main(String[] args) {
-		printAdd(10,15);
+		int x = 15;
+		int y = 10;
+		printAdd(x, y);
+		printSub(x, y);
 
 	}
 	
 	public static void printAdd(int arg1, int arg2) {
 //		引数で与えられた数値の合計を出力する
 		out.println(arg1 + arg2);
+	}
+	
+	public static void printSub(int arg1, int arg2) {
+		out.println(arg1 - arg2);
 	}
 
 }
